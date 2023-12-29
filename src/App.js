be3af2faf52c40d9c20b339ca307b2d1210cf556
Alibaba <abdulmanafaliu414@gmail.com>
2023-12-29 12:48:17 +0000

    i made i chages in two components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [itemName, setItemName] = useState("");
@@ -11,31 +12,64 @@ function App() {
       return;
     }
 
-    setStoredList([...storedList, itemName]);
+    setStoredList([...storedList, { text: itemName, isChecked: false }]);
     setItemName("");
   };
 
+  const handleCheckboxChange = (index) => {
+    const updatedList = [...storedList];
+    updatedList[index].isChecked = !updatedList[index].isChecked;
+    setStoredList(updatedList);
+  };
+
   return (
-    <div>
+    <div className="main-container">
       <form onSubmit={handleSubmit}>
-        <label>To do</label> <br />
+        <label>To do</label> <br /> <br />
         <input
+          style={{ padding: "5px 5px", fontSize: "15px" }}
           type="text"
           value={itemName}
           onChange={(e) => setItemName(e.target.value)}
-        />
-        <button type="submit">Submit</button>
+        />{" "}
+        <button
+          style={{ cursor: "pointer", padding: "5px 5px", fontSize: "15px" }}
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
-      {storedList.length > 0 ? (
-        <ul>
-          <h1>List items</h1>
-          {storedList.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      ) : (
-        "list is empty"
-      )}
+      <div className="listItems">
+        {storedList.length > 0 ? (
+          <div>
+            <h1>List items</h1>
+            <div
+              style={{
+                width: "150px",
+                textAlign: "start",
+                margin: "0 auto",
+              }}
+            >
+              <ol>
+                {storedList.map((item, index) => (
+                  <li key={index}>
+                    {item.text}{" "}
+                    <input
+                      type="checkbox"
+                      id={index}
+                      name="myReactCheckbox"
+                      checked={item.isChecked || false}
+                      onChange={() => handleCheckboxChange(index)}
+                    />
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        ) : (
+          " "
+        )}
+      </div>
     </div>
   );
 }
